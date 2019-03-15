611865e5aa25c020a0f389615564efd17841dfdc
chengfu <243712488@qq.com>
2019-03-15 17:36:14 +0800

    1.播放器屏幕旋转逻辑处理

---
+++ demo/src/main/java/com/chengfu/android/fuplayer/demo/ui/video/IBackPressed.java
@@ -0,0 +1,5 @@
+package com.chengfu.android.fuplayer.demo.ui.video;
+
+public interface IBackPressed {
+    boolean onBackPressed();
+}
--- demo/src/main/java/com/chengfu/android/fuplayer/demo/ui/video/VideoListActivity.java
+++ demo/src/main/java/com/chengfu/android/fuplayer/demo/ui/video/VideoListActivity.java
@@ -1,5 +1,7 @@
 package com.chengfu.android.fuplayer.demo.ui.video;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -8,22 +10,26 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.chengfu.android.fuplayer.demo.R;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class VideoListActivity extends AppCompatActivity {
+public class VideoListActivity extends AppCompatActivity implements IGetVideoContainer {
 
     private final static String TAG = "MediaSessionTest";
 
-    private final static String TABS[] = {"热门", "影视", "游戏", "搞笑"};
+    //    private final static String TABS[] = {"热门", "影视", "游戏", "搞笑"};
+    private final static String TABS[] = {"热门"};
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private FrameLayout videoContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,9 +45,28 @@ public class VideoListActivity extends AppCompatActivity {
         mTabLayout = findViewById(R.id.tabs);
         mTabLayout.setupWithViewPager(mViewPager);
 
+        videoContainer = findViewById(R.id.videoContainer);
+
     }
 
+    @Override
+    public void onBackPressed() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            return;
+        }
+        super.onBackPressed();
+    }
 
+    @Override
+    public ViewGroup getVideoContainer() {
+        return videoContainer;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
